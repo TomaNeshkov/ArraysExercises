87fbd249d8f009a29b08a0180674a56af9fc5662
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-16 21:10:49 +0300

    Exercise 6 (100/100)

---
+++ 6. Max Sequence of Equal Elements/6. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        int sequenceLength = 1;
+        int longestSequence = 0;
+        int longestSequenceNumber = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int currentNumber = numbers[i];
+
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                int nextNumber = numbers[j];
+
+                if (currentNumber == nextNumber)
+                {
+                    sequenceLength++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (sequenceLength > longestSequence)
+            {
+                longestSequence = sequenceLength;
+                longestSequenceNumber = numbers[i];
+            }
+
+            sequenceLength = 1;
+        }
+
+        for (int k = 0; k < longestSequence; k++)
+        {
+            Console.Write($"{longestSequenceNumber} ");
+        }
+    }
+}
