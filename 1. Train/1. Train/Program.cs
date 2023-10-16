92d39385680917adeed20cf3cfd20edf24b25cfb
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-16 21:04:49 +0300

    Exercise 1 (100/100)

---
+++ 1. Train/1. Train/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] train = new int[n];
+        int peopleCount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int peoplePerWagon = int.Parse(Console.ReadLine());
+            train[i] = peoplePerWagon;
+            peopleCount += peoplePerWagon;
+        }
+
+        Console.WriteLine(string.Join(" ", train));
+        Console.WriteLine(peopleCount);
+    }
+}
